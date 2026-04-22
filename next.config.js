22375044dd578479b10ce20e01b7592ee3a5bcaf
BoemoM5CS <mogaleboemo@gmail.com>
2026-04-22 11:13:52 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Env vars are read from .env.local (local) or deployment platform env vars (Vercel/Railway/etc.)
-  // Do NOT hardcode them here.
-  images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
